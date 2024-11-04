c0bdcdf759698a9a77e7b52c64a85d3e873cf52f
MrBAD610 <badbackgroundproductions@gmail.com>
2024-11-03 20:52:46 -0500

    Basic Movement Implimented
    I implemented a basic movement script with help from the following video:
    https://www.youtube.com/watch?v=lclDl-NGUMg

--- Little Starship/Assets/Scripts/PlayerController.cs
+++ Little Starship/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Look Sensitivity")]
+    [SerializeField] private float mouseSensitivity = 2.0f;
+
+    private Rigidbody rb;
+    private PlayerInputHandler inputHandler;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        inputHandler = PlayerInputHandler.Instance;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        horizontalInput = inputHandler.MoveInput.x;
+        verticalInput = inputHandler.MoveInput.y;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float speed = moveSpeed;
+        rb.velocity = new Vector3(horizontalInput * speed, 0f, verticalInput * speed);
     }
 }
