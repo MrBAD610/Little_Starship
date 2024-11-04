9619ef1fd0ced681d31317aa11766380df7eceed
MrBAD610 <badbackgroundproductions@gmail.com>
2024-11-03 19:21:30 -0500

    Set Up Unity's New Input System
    The player stand-in model has been created. I also installed Unity's new input system, created an input map, and created the "Player Input Handler" script all with the help of this tutorial video:
    https://www.youtube.com/watch?v=lclDl-NGUMg

---
+++ Little Starship/Assets/Scripts/AutomaticTileTexture.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AutomaticTileTexture : MonoBehaviour
+{
+    void Start()
+    {
+        var renderer = GetComponent<Renderer>();
+        var texture = new Material(renderer.sharedMaterial);
+        texture.mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.z);
+        renderer.sharedMaterial = texture;
+    }
+
+    void Update()
+    {
+        if (transform.hasChanged && Application.isEditor && !Application.isPlaying)
+        {
+            var renderer = GetComponent<Renderer>();
+            var texture = new Material(renderer.sharedMaterial);
+            texture.mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.z);
+            renderer.sharedMaterial = texture;
+        }
+    }
+}
