22291a8f45d02a5580d696abaf09db89b97b23c6
MrBAD610 <badbackgroundproductions@gmail.com>
2024-11-19 01:26:34 -0500

    Enemy References Script
    Using some YouTube videos and the structure of the "PlayerInputHandler" script as a guide, I created the "EnemyReferences" script which currently finds the player game object and tracks its location for all the enemies in a scene. Admittedly, I haven't made it in the same vein as the "PlayerInputHandler" script because I need to get more familiar with "singletons". For now, it works since multiple enemies can use it. I also discovered more efficient setup for baking NavMeshs (https://www.youtube.com/watch?v=HOAPvQONpsU) which I think I will be using from now on. These were my resources:
    
    - https://www.youtube.com/watch?v=u2EQtrdgfNs
    - https://www.youtube.com/watch?v=rs7xUi9BqjE
    - https://www.youtube.com/watch?v=HOAPvQONpsU
    - https://www.youtube.com/watch?v=UjkSFoLxesw

--- Little Starship/Assets/Scripts/EnemyNavigation.cs
+++ Little Starship/Assets/Scripts/EnemyNavigation.cs
@@ -5,22 +5,62 @@ using UnityEngine.AI;
 
 public class EnemyNavigation : MonoBehaviour
 {
-    public Transform playerLocation;
+    private GameObject player;
+    private Transform playerLocation;
     private NavMeshAgent agent;
+    private Transform agentLocation;
+
+    private EnemyReferences enemyReferences;
+
+    [Header("Stats")]
+    [SerializeField] public float pathUpdateDelay = 0.2f;
+    [SerializeField] public float detectionRadius = 5f;
+
+    private float pathUpdateDeadline;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool hasAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
 
     private void Awake()
     {
+        enemyReferences = GameObject.Find("Enemy References").GetComponent<EnemyReferences>();
+        player = enemyReferences.playerReference;
         agent = GetComponent<NavMeshAgent>();
     }
 
-    //void Start()
-    //{
-        
-    //}
+    private void Start()
+    {
+        playerLocation = enemyReferences.playerLocationReference;
+        agentLocation = transform;
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        agent.destination = playerLocation.position;
+        bool canDetect = Vector3.Distance(agentLocation.position, playerLocation.position) <= detectionRadius;
+
+        if (canDetect)
+        {
+            UpdatePath();
+        }
+    }
+
+    private void UpdatePath()
+    {
+        if (Time.time >= pathUpdateDeadline)
+        {
+            Debug.Log("Updating Path");
+            pathUpdateDeadline = Time.time + pathUpdateDelay;
+            agent.SetDestination(playerLocation.position);
+        }
     }
 }
