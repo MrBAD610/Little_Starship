c859d35b41da5734b798e42e0e34850cc2752e8d
MrBAD610 <badbackgroundproductions@gmail.com>
2024-11-21 20:26:05 -0500

    BodyRegion Enums
    Scripted the BodyRegion enum.

---
+++ Little Starship/Assets/Scripts/BodyRegion.cs
@@ -0,0 +1,11 @@
+public enum BodyRegion
+{
+    Generalized,  // medical emergency source not localized
+    Head,         // the head
+    Neck,         // the neck
+    Chest,        // the chest area
+    LeftArm,      // the left arm
+    RightArm,     // the right arm
+    LeftLeg,      // the left leg
+    RightLeg      // the right leg
+}
