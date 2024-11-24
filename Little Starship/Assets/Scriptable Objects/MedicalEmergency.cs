91a2c38e5603b26abf54e9692684556dc5d7b4f5
MrBAD610 <badbackgroundproductions@gmail.com>
2024-11-23 21:55:38 -0500

    Basic Medical Emergencies
    Created some basic Medical Emergencies and assigned them to colonists in the scene.

--- Little Starship/Assets/Scriptable Objects/MedicalEmergency.cs
+++ Little Starship/Assets/Scriptable Objects/MedicalEmergency.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MedicalEmergency : ScriptableObject
 {
     public string emergencyName;    // What the emergency is
-    public List<BodyRegion> affectedRegions; // regions affected by the medical emergency
+    public List<BodyRegion> presetAffectedRegions; // list of preset regions affected by the medical emergency
+    public List<BodyRegion> randomAffectedRegions; // list of possible regions affected by the medical emergency
     public float stabilizationTime; // Time to stabilize this emergency
 }
